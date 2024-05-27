00f7600f7a87c1b96a39171bb119cf61c85c11e8
koya-ido <koya.development0@gmail.com>
2024-05-27 18:26:54 +0900

    API繋ぎこみ

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,27 @@
 <template>
   <h1>{{ headerTitle }}</h1>
+  <button @click="onClick(0)">ボタンA</button>
+  <button @click="onClick(1)">ボタンB</button>
+  <p>{{ viewResult }}</p>
 </template>
 <script setup lang="ts">
+  interface lotteryResult {
+    num: number;
+    result: boolean;
+  }
+
   const headerTitle = ref<string>("Hello World");
+  let viewResult = ref<string>("");
+  
+  const onClick = (num: number) => {
+    lottery(num);
+  }
+
+  const lottery = async (num: number) => {
+    const {data: response} = await useFetch<lotteryResult>(`http://localhost:8080/lottery/${num}`);
+    if (response.value) {
+      viewResult.value = response.value.result ? "あたり" : "はずれ";
+    }
+  }
+  
 </script>
